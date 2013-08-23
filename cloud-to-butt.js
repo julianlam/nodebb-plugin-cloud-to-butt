aae94e04edc8d182ae1c0c84712177a7bb31c38f
psychobunny <psycho.bunny@hotmail.com>
2013-08-24 01:41:19 +0800

    Revert "linted and OPTIMIZED"
    This reverts commit 02a2805c81ce0e54e322423f0b007c79f8e933a2.

--- cloud-to-butt.js
+++ cloud-to-butt.js
@@ -1,16 +1,13 @@
-(function() {
-	"use strict";
-	/*global module*/
+var	ctb = {
+	filter: function(text) {
+		text = text.replace(/\bThe Cloud\b/g, "My Butt");
+		text = text.replace(/\bThe cloud\b/g, "My butt");
+		text = text.replace(/\bthe Cloud\b/g, "my Butt");
+		text = text.replace(/\bthe cloud\b/g, "my butt");
+		text = text.replace(/\bTHE CLOUD\b/g, "MY BUTT");
 
-	var	ctb = {
-		filter: function(text) {
-			return text.replace(/\bThe Cloud\b/g, "My Butt")
-						.replace(/\bThe cloud\b/g, "My butt")
-						.replace(/\bthe Cloud\b/g, "my Butt")
-						.replace(/\bthe cloud\b/g, "my butt")
-						.replace(/\bTHE CLOUD\b/g, "MY BUTT");
-		}
-	};
+		return text;
+	}
+}
 
-	module.exports = ctb;
-}());
+module.exports = ctb;
